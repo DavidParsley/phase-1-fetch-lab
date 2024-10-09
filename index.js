d02e4bc33613b53ec0a4c77ad73a8a56458e390f
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-10-09 12:57:05 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   return fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
+  .then((res) => res.json())
   .then(data=>{renderBooks(data)});
 }
 
